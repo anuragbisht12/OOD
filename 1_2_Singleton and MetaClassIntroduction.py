19197b839926973c993fb26d86c80ad84f0c2b38
anuragbisht12 <anuragbisht12@gmail.com>
2020-07-05 16:28:10 +0530

    secondcommit

--- 1_2_Singleton and MetaClassIntroduction.py
+++ 1_2_Singleton and MetaClassIntroduction.py
@@ -45,9 +45,76 @@ print(log1,log2)
 
 
 # *******************************************Actual Application ***********************************************
+# Database read write objects application
+
+import sqlite3
+class MetaSingleton(type):
+    _instances={}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls]= super(MetaSingleton,cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Database(metaclass=MetaSingleton):
+    connection=None
+    def connect(self):
+        if self.connection is None:
+            self.connection=sqlite3.connect('db.sqlite3')
+            self.cursorobj=self.connection.cursor()
+        return self.cursorobj
+
+if __name__ == "__main__":
+    db1=Database().connect()
+    db2=Database().connect()
+
+    print("Database objects created DB1: ", db1)
+    print("Database objects created DB1: ", db2)
 
 
 
+# ******************Application 2: Health check Service**********************************
+
+class HealthCheck:
+    _instance =None
+    def __new__(cls,*args,**kwargs):
+        if not HealthCheck._instance:
+            HealthCheck._instance=super(HealthCheck,cls).__new__(cls, *args, **kwargs)
+        return HealthCheck._instance
+    
+    def __init__(self):
+        self._servers=[]
+
+    def addServer(self):
+        """Add new server"""
+        self._servers.append("Server 1")
+        self._servers.append("Server 2")
+        self._servers.append("Server 3")
+        self._servers.append("Server 4")
+
+    def changeServer(self):
+        """
+        Change the server
+        """
+        self._servers.pop()
+        self._servers.append("Server 5")
+
+
+if __name__ == "__main__":
+    hc1=HealthCheck()
+    hc2=HealthCheck()
+
+    hc1.addServer()
+    print("Schedule health check for servers (1)..")
+    for i in range(4):
+        print("Checking ", hc1._servers[i])
+
+    hc2.changeServer()
+    print("Schedule health check for servers (2)..")
+    for i in range(4):
+        print("Checking ", hc2._servers[i])
+
+
+    
 
 
 
